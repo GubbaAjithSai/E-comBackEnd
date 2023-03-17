495206022d94e2b8d4e4fb24cd5a26729a551d42
gubbaajithsai <gubbaajithsai@gmail.com>
2023-03-17 13:00:16 +0530

    changes

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express=require("express");
 const app=express();
 dotenv.config({path:'./config.env'});
 require("./db/conn");
+
+const PORT=process.env.PORT || 3000
 app.use(express.json())
 app.use(require("./routers/auth"));
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("server connected...");
 })
