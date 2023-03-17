4f0f0331861da265676ac6823a52baa736fa105d
gubbaajithsai <gubbaajithsai@gmail.com>
2023-03-17 23:45:55 +0530

    changes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const dotenv=require("dotenv");
 const express=require("express");
 const app=express();
+const cors = require("cors");
 dotenv.config({path:'./config.env'});
 require("./db/conn");
-
 const PORT=process.env.PORT || 3000
+app.use(cors());
 app.use(express.json())
 app.use(require("./routers/auth"));
 
